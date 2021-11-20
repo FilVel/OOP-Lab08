853453de4fb92fd71887ef4714bec730771eae0f
Filippo Velli <filippo.velli@studio.unibo.it>
2021-11-20 21:47:58 +0100

    Controller.java and SimpleGUI.java of the mvcio excercise completed and .gitignore files of all the completed excercises have been added

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 /**
  * 
  */
@@ -8,19 +11,56 @@ public class Controller {
     /*
      * This class must implement a simple controller responsible of I/O access. It
      * considers a single file at a time, and it is able to serialize objects in it.
-     * 
-     * Implement this class with:
+     */
+
+    private static final String USERHOME = System.getProperty("user.home");
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String FILE = "output.txt";
+
+     /* Implement this class with:
      * 
      * 1) A method for setting a File as current file
-     * 
-     * 2) A method for getting the current File
-     * 
-     * 3) A method for getting the path (in form of String) of the current File
-     * 
-     * 4) A method that gets a String as input and saves its content on the current
+     */
+
+    private File currentFile = new File(USERHOME + SEPARATOR + FILE); 
+
+    /* 2) A method for getting the current File
+     */
+
+    /**
+     * @return the current file
+     */
+    public File getCurrentFile() {
+        return currentFile;
+    }
+
+    /* 3) A method for getting the path (in form of String) of the current File
+     */
+
+    /**
+     * @return the path (in String form) of the current file
+     */
+    public String getCurrentFilePath() {
+        return currentFile.getPath();
+    }
+
+    /* 4) A method that gets a String as input and saves its content on the current
      * file. This method may throw an IOException.
-     * 
-     * 5) By default, the current file is "output.txt" inside the user home folder.
+     */
+
+    /**
+     * @param string
+     *        the string that has to be saved in the current file
+     */
+    public void saveNewString(final String string) {
+       try (PrintStream destFile = new PrintStream(currentFile)) {
+           destFile.println(string);
+       } catch (IOException e) {
+           System.out.println("Something has gone wrong while saving");
+       }
+    }
+
+    /* 5) By default, the current file is "output.txt" inside the user home folder.
      * A String representing the local user home folder can be accessed using
      * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
      * Windows) can be obtained as String through the method
--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -2,8 +2,17 @@ package it.unibo.oop.lab.mvcio;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.LayoutManager;
+import java.awt.BorderLayout;
 
 /**
  * A very simple program using a graphical interface.
@@ -11,7 +20,7 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first Java Grafical Interface");
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -35,14 +44,34 @@ public final class SimpleGUI {
 
     /**
      * builds a new {@link SimpleGUI}.
+     * 
+     * @param controller
      */
-    public SimpleGUI() {
+    public SimpleGUI(final Controller controller) {
+
+        final JTextArea text = new JTextArea();
+        final JPanel panel = new JPanel();
+        final LayoutManager layout = new BorderLayout();
+        panel.setLayout(layout);
+
+        panel.add(text, BorderLayout.CENTER);
+        final JButton button = new JButton("Save");
+        panel.add(button, BorderLayout.SOUTH);
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                    controller.saveNewString(text.getText());
+            }
+        });
+        frame.setContentPane(panel);
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
          * primary is selected.
-         * 
-         * In order to deal coherently with multimonitor setups, other
+         */
+
+        /* In order to deal coherently with multimonitor setups, other
          * facilities exist (see the Java documentation about this issue). It is
          * MUCH better than manually specify the size of a window in pixel: it
          * takes into account the current resolution.
@@ -57,6 +86,12 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... strings) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
     }
 
 }
